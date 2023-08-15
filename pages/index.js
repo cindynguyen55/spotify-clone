dd8b9d2d4f968d851e49dc96c36b5e36ef8971fe
20120519 <lieunguyenthi1802@gmail.com>
2023-08-16 02:51:58 +0700

    config next-auth

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,34 @@
 import { useSession } from "next-auth/react"
+import { useEffect, useState } from "react"
 
 export default function Home() {
 
-  // const {data: session} = useSession()
+  const { data: session } = useSession()
+  const [x, setX] = useState('')
   // console.log(session.user.accessToken)
+  const [playlists, setPlaylists] = useState([])
+
+  useEffect(() => {
+    async function f() {
+      if (session && session.accessToken) {
+        setX(session.accessToken)
+
+        const res = await fetch('https://api.spotify.com/v1/me/playlists',{
+          headers: {
+            Authorization: `Bearer ${session.accessToken}`
+          }
+        })
+
+        const data = await res.json()
+        setPlaylists(data.items)
+      }
+    }
+    f()
+  }, [session])
 
   return (
     <main className="">
-      
+
     </main>
   )
 }
